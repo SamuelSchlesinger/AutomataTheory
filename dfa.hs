c616d8928a8d0c4aa36ed4d6e85dd77cf42b41b6
Samuel Schlesinger <sgschlesinger@gmail.com>
2016-05-29 01:15:52 -0400

    fixed the types for a couple functions I had to flip

--- dfa.hs
+++ dfa.hs
@@ -1,32 +1,36 @@
 -- | Original author: Kit Fredurra
 -- | Some refactorings to "haskellify" the code
+-- | From the paper by Armando B. Matos
+-- |     Monoid machines: a O(log n) parser for regular languages.
+-- |    (http://www.dcc.fc.up.pt/~acm/semigr.pdf)
 
 import Control.Monad
 import Control.Parallel.Strategies
 
 data DFA symbol state = DFA {
-    states   :: [state] 
+    states   :: [state]
   , start    :: state
-  , delta    :: state -> symbol -> Maybe state 
+  , delta    :: symbol -> state -> Maybe state
   , accept   :: Elem state -> Bool -- g in the paper
 }
 
 type Elem state = state -> Maybe state
 
-createElem :: symbol -> DFA symbol state -> Elem state
-createElem s dfa = flip (delta dfa) s
+-- | Creates a monoid element corresponding to the given symbol
+createElem :: DFA symbol state -> symbol -> Elem state
+createElem dfa s = (delta dfa) s
 
+-- | Composition of elements in the generated monoid
 mmul :: Elem state -> Elem state -> Elem state
 f `mmul` g = f >=> g
 
+-- | Converts a word to its monoidal representation
 fword :: [symbol] -> DFA symbol state -> [Elem state]
-fword xs dfa = map ((flip createElem) dfa) xs
+fword xs dfa = map (createElem dfa) xs
 
 mparse :: [Elem state] -> DFA symbol state -> Bool
 mparse [x] dfa = (accept dfa) x
 mparse (x:y:xs) dfa = mparse ((x `mmul` y):xs) dfa
 
-
 parse :: [symbol] -> DFA symbol state -> Bool
 parse s dfa = mparse (fword s dfa) dfa
-
