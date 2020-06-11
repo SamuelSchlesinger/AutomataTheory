f53e186c3278ee0086830da029bb074f47566627
Samuel Schlesinger <samuel@simspace.com>
2020-06-11 01:49:52 -0400

    refactored some stuff

--- dfa.hs
+++ dfa.hs
@@ -1,13 +1,55 @@
-data DFA state symbol val = DFA {
-    dtransition :: symbol -> state -> state
-  , dvalue      :: state  -> val
-  , dstarting   :: state
-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module ComputationalMachines where
 
-evalDFA :: DFA state symbol val -> [symbol] -> val
-evalDFA dfa s = f dfa s (dstarting dfa) where
-    f dfa [] state = (dvalue dfa) state
-    f dfa (x:xs) state = f dfa xs (dtransition dfa x state)
+type DFA seq state symbol val = (symbol -> state -> state) -> (state -> val) -> state -> seq symbol -> val
 
-test :: DFA String Char String
-test = DFA (\x s -> (x:s)) reverse []
+dfa :: forall symbol state val.(symbol -> state -> state) -> (state -> val) -> state -> [symbol] -> val
+dfa transition value starting s = f s starting where
+    f :: [symbol] -> state -> val
+    f [] state = value state
+    f (x:xs) state = f xs (transition x state)
+
+ndfa :: forall state symbol val n. 
+    Monad n
+ => (symbol -> state -> n state)
+ -> (state -> val) 
+ -> state 
+ -> [symbol]
+ -> n val
+ndfa transition value starting s = f s starting where
+  f :: [symbol] -> state -> n val
+  f [] state = pure (value state)
+  f (x:xs) state = do
+    state' <- transition x state
+    f xs state'
+
+tm :: forall tape state symbol val.
+    (state -> symbol -> (forall x. tape x -> (tape x, state)))
+ -> (forall x. tape x -> x)
+ -> (state -> val)
+ -> (state -> Bool)
+ -> state
+ -> tape symbol
+ -> val
+tm transition read value finished state tape = f tape state where
+  f :: tape symbol -> state -> val
+  f tape state = if finished state then value state else uncurry f (transition state (read tape) tape)
+
+ntm :: forall n tape state symbol val.
+    Monad n
+ => (state -> symbol -> (forall x. tape x -> n (tape x, state)))
+ -> (forall x. tape x -> x)
+ -> (state -> val)
+ -> (state -> Bool)
+ -> state
+ -> tape symbol
+ -> n val
+ntm transition read value finished state tape = f tape state where
+  f :: tape symbol -> state -> n val
+  f tape state =
+    if finished state
+      then pure (value state)
+      else do
+        (tape', state') <- transition state (read tape) tape
+        f tape' state'
